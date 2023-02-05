614b8e0eab5cd991d3d9fef22c97439c00158e69
David Scicluna <dave_nostra@hotmail.com>
2023-02-05 19:33:36 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	base: '/',
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+	build: { outDir: 'build' },
+	server: { open: false, port: 3000 }
+});
