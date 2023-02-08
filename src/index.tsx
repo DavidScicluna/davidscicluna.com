ac65aad1f92a5d59b284147f93d7c114976ca368
DavidScicluna <dave_nostra@hotmail.com>
2023-02-08 21:29:50 +0100

    Fix: root index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import { StrictMode } from 'react';
 
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+import Providers from './containers/Providers';
+
+// Importing Main Fonts (Raleway)
+import '@fontsource/raleway/100.css';
+import '@fontsource/raleway/100-italic.css';
+import '@fontsource/raleway/200.css';
+import '@fontsource/raleway/200-italic.css';
+import '@fontsource/raleway/300.css';
+import '@fontsource/raleway/300-italic.css';
+import '@fontsource/raleway/400.css';
+import '@fontsource/raleway/400-italic.css';
+import '@fontsource/raleway/500.css';
+import '@fontsource/raleway/500-italic.css';
+import '@fontsource/raleway/600.css';
+import '@fontsource/raleway/600-italic.css';
+import '@fontsource/raleway/700.css';
+import '@fontsource/raleway/700-italic.css';
+import '@fontsource/raleway/800.css';
+import '@fontsource/raleway/800-italic.css';
+import '@fontsource/raleway/900.css';
+import '@fontsource/raleway/900-italic.css';
+// Importing Main Fonts (Roboto Slab)
+import '@fontsource/roboto-slab/100.css';
+import '@fontsource/roboto-slab/200.css';
+import '@fontsource/roboto-slab/300.css';
+import '@fontsource/roboto-slab/400.css';
+import '@fontsource/roboto-slab/500.css';
+import '@fontsource/roboto-slab/600.css';
+import '@fontsource/roboto-slab/700.css';
+import '@fontsource/roboto-slab/800.css';
+import '@fontsource/roboto-slab/900.css';
+// Importing Main Fonts (Roboto Mono)
+import '@fontsource/roboto-mono/100.css';
+import '@fontsource/roboto-mono/100-italic.css';
+import '@fontsource/roboto-mono/200.css';
+import '@fontsource/roboto-mono/200-italic.css';
+import '@fontsource/roboto-mono/300.css';
+import '@fontsource/roboto-mono/300-italic.css';
+import '@fontsource/roboto-mono/400.css';
+import '@fontsource/roboto-mono/400-italic.css';
+import '@fontsource/roboto-mono/500.css';
+import '@fontsource/roboto-mono/500-italic.css';
+import '@fontsource/roboto-mono/600.css';
+import '@fontsource/roboto-mono/600-italic.css';
+import '@fontsource/roboto-mono/700.css';
+import '@fontsource/roboto-mono/700-italic.css';
+
+// Importing Material UI Icons
+import '@fontsource/material-icons';
+import '@fontsource/material-icons-outlined';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement as HTMLElement);
 
 root.render(
-	<React.StrictMode>
-		<h1>Hello World!</h1>
-	</React.StrictMode>
+	<StrictMode>
+		<Providers />
+	</StrictMode>
 );
