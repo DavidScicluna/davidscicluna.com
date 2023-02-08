42c5b533e16c318b3b1ae65af77515025237f236
DavidScicluna <dave_nostra@hotmail.com>
2023-02-08 21:19:39 +0100

    Feature: Providers

---
+++ src/containers/Providers/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+
+import { DSCLProvider } from '@davidscicluna/component-library';
+
+import { Provider as ReduxProvider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import store from '../../store';
+import theme from '../../theme';
+import Container from '../Container';
+
+const persistor = persistStore(store);
+
+const Providers: FC = () => {
+	return (
+		<ReduxProvider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<DSCLProvider theme={theme}>
+					<Container />
+				</DSCLProvider>
+			</PersistGate>
+		</ReduxProvider>
+	);
+};
+
+export default Providers;
