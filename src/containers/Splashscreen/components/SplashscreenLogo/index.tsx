ac0ac59c1359021099e2c218399c917347c3c7d1
DavidScicluna <dave_nostra@hotmail.com>
2023-02-08 21:28:29 +0100

    Feature: Splashscreen - SplashscreenLogo

---
+++ src/containers/Splashscreen/components/SplashscreenLogo/index.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react';
+
+import { useTheme, ScaleFade, utils } from '@davidscicluna/component-library';
+
+import { useConst, Text } from '@chakra-ui/react';
+
+import { Transition } from 'framer-motion';
+
+import { useUserTheme } from '../../../../common/hooks';
+
+const { getTransitionConfig, getTransitionDuration } = utils;
+
+const { getColor } = utils;
+
+const SplashscreenLogo: FC = () => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	const duration = useConst<number>(getTransitionDuration({ theme, duration: 'slow' }));
+	const config = useConst<Transition>({ ...getTransitionConfig({ theme }), duration });
+
+	return (
+		<ScaleFade in transition={{ enter: { ...config }, exit: { ...config } }}>
+			<Text
+				align='center'
+				color={getColor({ theme, colorMode, type: 'text.primary' })}
+				fontSize={['3xl', '4xl', '5xl', '6xl', '7xl', '8xl']}
+				fontWeight='bold'
+				userSelect='none'
+				whiteSpace='nowrap'
+				textTransform='lowercase'
+				sx={{
+					'@keyframes logo-tracking': {
+						'0%': {
+							letterSpacing: '-0.5em',
+							transform: 'translateZ(-700px)',
+							opacity: 0
+						},
+						'50%': { opacity: 0 },
+						'100%': { opacity: 1, transform: 'translateZ(0)' }
+					},
+
+					'animationName': 'logo-tracking',
+					'animationDuration': '2.5s',
+					'animationFillMode': 'both',
+					'animationTimingFunction': theme.transition.easing['ease-in-out']
+				}}
+			>
+				davidscicluna
+			</Text>
+		</ScaleFade>
+	);
+};
+
+export default SplashscreenLogo;
