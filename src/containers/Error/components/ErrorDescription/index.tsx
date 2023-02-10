1d4a52c022250f9f792864176edf526665dc2cce
DavidScicluna <dave_nostra@hotmail.com>
2023-02-10 11:30:40 +0100

    Feature: Error - ErrorDescription

---
+++ src/containers/Error/components/ErrorDescription/index.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+
+import { useTheme, utils } from '@davidscicluna/component-library';
+
+import { VStack, Text } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../common/hooks';
+
+import { ErrorDescriptionProps } from './types';
+
+const { getColor } = utils;
+
+const ErrorDescription: FC<ErrorDescriptionProps> = ({ title, subtitle }) => {
+	const theme = useTheme();
+	const { colorMode } = useUserTheme();
+
+	return (
+		<VStack alignItems='flex-start' justifyContent='center' spacing={0.5}>
+			<Text
+				align='left'
+				color={getColor({ theme, colorMode, type: 'text.primary' })}
+				fontSize='4xl'
+				fontWeight='bold'
+				lineHeight='shorter'
+			>
+				{title}
+			</Text>
+			<Text
+				align='left'
+				color={getColor({ theme, colorMode, type: 'text.secondary' })}
+				fontSize='md'
+				lineHeight='shorter'
+			>
+				{subtitle}
+			</Text>
+		</VStack>
+	);
+};
+
+export default ErrorDescription;
