af9e2883fa3f07fe1eb7526f12161939b127f7d5
DavidScicluna <dave_nostra@hotmail.com>
2023-02-10 11:30:28 +0100

    Feature: Error - ErrorCode

---
+++ src/containers/Error/components/ErrorCode/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import { useTheme, utils } from '@davidscicluna/component-library';
+
+import { Text } from '@chakra-ui/react';
+
+import { useUserTheme } from '../../../../common/hooks';
+
+import { ErrorCodeProps } from './types';
+
+const { getColor } = utils;
+
+const ErrorCode: FC<ErrorCodeProps> = ({ code }) => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	return (
+		<Text
+			align='right'
+			color={getColor({ theme, colorMode, color, type: 'color' })}
+			fontSize='9xl'
+			fontWeight='extrabold'
+			lineHeight='1'
+		>
+			{code}
+		</Text>
+	);
+};
+
+export default ErrorCode;
