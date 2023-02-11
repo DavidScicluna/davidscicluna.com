5f0460ea91905208eb48e3ef8a3b7d907c5dc4d6
DavidScicluna <dave_nostra@hotmail.com>
2023-02-11 19:32:35 +0100

    Fix: Container - Made Splashscreen visible for all devices

--- src/containers/Container/index.tsx
+++ src/containers/Container/index.tsx
@@ -1,39 +1,46 @@
-import { FC } from 'react';
+import { FC, lazy } from 'react';
 
-import { useTheme, utils } from '@davidscicluna/component-library';
-
-import { useMediaQuery, useBoolean } from '@chakra-ui/react';
+import { useBoolean } from '@chakra-ui/react';
 
 import { useIsFirstRender, useTimeout } from 'usehooks-ts';
 
+import { useCheckColorMode } from '../../common/hooks';
+import { Suspense } from '../../components';
+import DummyLayout from '../Layout/DummyLayout';
 import Router from '../Router';
 import Routes from '../Routes';
 import Splashscreen from '../Splashscreen';
 
-const { checkIsTouchDevice } = utils;
-
-const isTouchDevice = checkIsTouchDevice();
+const Layout = lazy(() => import('../Layout/OriginalLayout'));
 
 const Container: FC = () => {
-	const theme = useTheme();
-
-	const [isLg] = useMediaQuery(`(max-width: ${theme.breakpoints.lg})`);
-
 	const isFirstRender = useIsFirstRender();
 
 	const [isSplashscreenOpen, setSetIsSplashscreenOpen] = useBoolean(isFirstRender);
 	const [isRoutesVisible, setSetIsRoutesVisible] = useBoolean();
 
-	useTimeout(() => setSetIsSplashscreenOpen.off(), isSplashscreenOpen ? 10000 : null);
-	useTimeout(() => setSetIsRoutesVisible.on(), isLg && isTouchDevice ? (isSplashscreenOpen ? 5000 : null) : 0);
+	useCheckColorMode();
+
+	useTimeout(() => setSetIsSplashscreenOpen.off(), isSplashscreenOpen ? 5000 : null);
+	useTimeout(() => setSetIsRoutesVisible.on(), isSplashscreenOpen ? 4000 : null);
 
 	return (
 		<Router>
-			{isLg && isTouchDevice && (
-				<Splashscreen isOpen={isSplashscreenOpen} onClose={() => setSetIsSplashscreenOpen.off()} />
-			)}
+			<Splashscreen isOpen={isSplashscreenOpen} onClose={() => setSetIsSplashscreenOpen.off()} />
 
-			{isRoutesVisible && <Routes />}
+			{isRoutesVisible && (
+				<Suspense
+					fallback={
+						<DummyLayout>
+							<Routes />
+						</DummyLayout>
+					}
+				>
+					<Layout>
+						<Routes />
+					</Layout>
+				</Suspense>
+			)}
 		</Router>
 	);
 };
