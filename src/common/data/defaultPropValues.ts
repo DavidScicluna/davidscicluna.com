611eece9d0a77361fcc3fd8f104782ab841aaf8e
DavidScicluna <dave_nostra@hotmail.com>
2023-02-12 18:36:08 +0100

    Fix: common defaultPropValues

--- src/common/data/defaultPropValues.ts
+++ src/common/data/defaultPropValues.ts
@@ -2,3 +2,5 @@ import { AppColor, AppColorMode } from '../../store/slices/App/types';
 
 export const color: AppColor = 'blue';
 export const colorMode: AppColorMode = 'system';
+
+export const inView = true;
