01d92b7e24a555407b84359699cb5b13c270c9ed
DavidScicluna <dave_nostra@hotmail.com>
2023-02-12 19:01:36 +0100

    Feature: Email Icon

--- src/common/assets/icons/index.ts
+++ src/common/assets/icons/index.ts
@@ -1,6 +1,7 @@
 import dark from './dark';
+import Email from './Email';
 import Github from './Github';
 import light from './light';
 import Linkedin from './Linkedin';
 
-export default { dark, Github, light, Linkedin };
+export default { Email, dark, Github, light, Linkedin };
