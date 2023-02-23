48abdb6a2162efc31066e66f4f8fd3e2000ea943
DavidScicluna <dave_nostra@hotmail.com>
2023-02-23 18:34:02 +0100

    Feature: Layout - OriginalLayout - Navigation - NavigationItems

---
+++ src/containers/Layout/OriginalLayout/components/Navigation/components/NavigationItems/index.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+
+import { useLocation } from 'react-router';
+
+import { useTheme, InternalLink, Button } from '@davidscicluna/component-library';
+
+import { useMediaQuery, HStack } from '@chakra-ui/react';
+
+import navItems from '../../../../../../../common/content/navItems';
+import { useUserTheme } from '../../../../../../../common/hooks';
+
+const NavigationItems: FC = () => {
+	const theme = useTheme();
+	const { color, colorMode } = useUserTheme();
+
+	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+	const [isMd] = useMediaQuery(`(max-width: ${theme.breakpoints.md})`);
+
+	const location = useLocation();
+
+	return (
+		<HStack spacing={0}>
+			{navItems.map(({ title, location: l, getIsActive }, index) => {
+				const isActive = getIsActive(location);
+				return (
+					<InternalLink key={index} to={{ ...l }} isDisabled={isActive}>
+						<Button
+							color={isActive ? color : 'gray'}
+							colorMode={colorMode}
+							size={isSm ? 'xs' : isMd ? 'sm' : 'md'}
+							variant='text'
+						>
+							{title}
+						</Button>
+					</InternalLink>
+				);
+			})}
+		</HStack>
+	);
+};
+
+export default NavigationItems;
