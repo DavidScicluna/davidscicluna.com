627c1eb292b943a0d12138bd1e425d768a433fa4
DavidScicluna <dave_nostra@hotmail.com>
2023-02-26 19:01:25 +0100

    Feature: ENV - Moved email url to env

--- src/components/MailOverlay/index.tsx
+++ src/components/MailOverlay/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { ExternalLink, LinkProps as MailOverlayProps } from '@davidscicluna/component-library';
 
-const email = 'hello@davidscicluna.com';
+const email = import.meta.env.VITE_EMAIL_URL;
 
 const MailOverlay: FC<MailOverlayProps> = ({ children }) => {
 	return <ExternalLink href={`mailto:${email}`}>{children}</ExternalLink>;
