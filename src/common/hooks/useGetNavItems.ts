2ee4a6bf3028120ccf7587d1248700f70a2d156b
DavidScicluna <dave_nostra@hotmail.com>
2023-02-26 19:17:17 +0100

    Fix: useGetNavItems - Added useMemo

--- src/common/hooks/useGetNavItems.ts
+++ src/common/hooks/useGetNavItems.ts
@@ -1,28 +1,34 @@
+import { useMemo } from 'react';
+
 import { useTranslation } from 'react-i18next';
 
 type NavItem = { title: string; location: Partial<Location>; getIsActive: (location: Partial<Location>) => boolean };
 type NavItems = NavItem[];
 
 const useGetNavItems = (): NavItems => {
-	const { t } = useTranslation();
+	const { i18n, t } = useTranslation();
 
-	return [
-		{
-			title: `${t('common.navItems./')}`,
-			location: { pathname: '/' },
-			getIsActive: ({ pathname }) => pathname === '/'
-		},
-		{
-			title: `${t('common.navItems./about')}`,
-			location: { pathname: '/about' },
-			getIsActive: ({ pathname }) => pathname === '/about'
-		},
-		{
-			title: `${t('common.navItems./projects')}`,
-			location: { pathname: '/projects' },
-			getIsActive: ({ pathname }) => pathname === '/projects'
-		}
-	];
+	const navItems = useMemo<NavItems>(() => {
+		return [
+			{
+				title: `${t('common.navItems./')}`,
+				location: { pathname: '/' },
+				getIsActive: ({ pathname }) => pathname === '/'
+			},
+			{
+				title: `${t('common.navItems./about')}`,
+				location: { pathname: '/about' },
+				getIsActive: ({ pathname }) => pathname === '/about'
+			},
+			{
+				title: `${t('common.navItems./projects')}`,
+				location: { pathname: '/projects' },
+				getIsActive: ({ pathname }) => pathname === '/projects'
+			}
+		];
+	}, [i18n.language]);
+
+	return navItems;
 };
 
 export default useGetNavItems;
