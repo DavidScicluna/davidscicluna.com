c5d7e1d60ded1dcb098f2aa1f706d63991232c19
DavidScicluna <dave_nostra@hotmail.com>
2023-03-01 23:01:07 +0100

    Feature: banner index

---
+++ src/common/assets/banner/index.ts
@@ -0,0 +1,7 @@
+import sm from './480x270';
+import md from './720x405';
+import lg from './960x540';
+import xs from './96x54';
+import xl from './original';
+
+export default { sm, md, lg, xs, xl };
