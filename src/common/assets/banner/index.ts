6d88f58e86bb48e74c675900a3e7684e49773861
DavidScicluna <dave_nostra@hotmail.com>
2023-03-02 22:01:09 +0100

    Feature: 320x180 banner

--- src/common/assets/banner/index.ts
+++ src/common/assets/banner/index.ts
@@ -1,7 +1,8 @@
+import xs from './320x180';
 import sm from './480x270';
 import md from './720x405';
 import lg from './960x540';
-import xs from './96x54';
+import thumbnail from './96x54';
 import xl from './original';
 
-export default { sm, md, lg, xs, xl };
+export default { xs, sm, md, lg, xl, thumbnail };
