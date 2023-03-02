473eba5e1d1926362d9813cabcd808e0f45f87ac
DavidScicluna <dave_nostra@hotmail.com>
2023-03-02 22:02:57 +0100

    Feature: original portrait

---
+++ src/common/assets/portrait/index.ts
@@ -0,0 +1,8 @@
+import xs from './360';
+import sm from './480';
+import md from './720';
+import lg from './880';
+import thumbnail from './96';
+import xl from './original';
+
+export default { xs, sm, md, lg, xl, thumbnail };
