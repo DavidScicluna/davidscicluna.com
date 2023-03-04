d1c4c75165248cc70e9bacadaca02da547f1b675
DavidScicluna <dave_nostra@hotmail.com>
2023-03-04 15:34:51 +0100

    Fix: Layout - Footer - FooterDescription

--- src/containers/Layout/components/Footer/components/FooterDescription/index.tsx
+++ src/containers/Layout/components/Footer/components/FooterDescription/index.tsx
@@ -4,32 +4,33 @@ import { useTheme, utils } from '@davidscicluna/component-library';
 
 import { Text, VStack } from '@chakra-ui/react';
 
+import parser from 'react-html-parser';
+
 import { useGetAbout, useUserTheme } from '../../../../../../common/hooks';
 
 const { getColor } = utils;
 
 const FooterDescription: FC = () => {
 	const theme = useTheme();
-	const { colorMode } = useUserTheme();
+	const { color, colorMode } = useUserTheme();
 
 	const paragraphs = useGetAbout();
 
 	return (
 		<VStack width='100%' alignItems='stretch' justifyContent='stretch' spacing={2}>
-			{paragraphs
-				.filter((_paragraph, index) => index < paragraphs.length - 1)
-				.map((paragraph, index) => (
-					<Text
-						key={index}
-						align='left'
-						color={getColor({ theme, colorMode, type: 'text.secondary' })}
-						fontSize='sm'
-						lineHeight='shorter'
-						userSelect='none'
-					>
-						{paragraph}
-					</Text>
-				))}
+			{paragraphs.map((paragraph, index) => (
+				<Text
+					key={index}
+					align='left'
+					color={getColor({ theme, colorMode, type: 'text.secondary' })}
+					fontSize='sm'
+					lineHeight='shorter'
+					userSelect='none'
+					sx={{ '& a': { color: getColor({ theme, colorMode, color, type: 'color' }) } }}
+				>
+					{parser(paragraph)}
+				</Text>
+			))}
 		</VStack>
 	);
 };
