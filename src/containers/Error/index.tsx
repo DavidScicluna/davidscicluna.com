b899e9cc39d1129fecd46866e4fc88088a023398
DavidScicluna <dave_nostra@hotmail.com>
2023-06-29 22:14:59 +0200

    Fix: Error

--- src/containers/Error/index.tsx
+++ src/containers/Error/index.tsx
@@ -1,49 +1,46 @@
 import { FC } from 'react';
 
-import { Space, useTheme, Divider } from '@davidscicluna/component-library';
+import { useTheme, Divider } from '@davidscicluna/component-library';
 
 import { Center, Show, HStack, VStack } from '@chakra-ui/react';
 
-import { useSpacing, useUserTheme } from '../../common/hooks';
+import { useSpacing } from '../../common/hooks';
 
-import Code from './components/ErrorCode';
-import Description from './components/ErrorDescription';
-import { ErrorProps } from './types';
-
-const padding: Space = 2;
+import { ErrorProps } from './common/types';
+import ErrorCode from './components/ErrorCode';
+import ErrorDescription from './components/ErrorDescription';
 
 const Error: FC<ErrorProps> = (props) => {
 	const theme = useTheme();
-	const { color, colorMode } = useUserTheme();
 
-	const spacing = useSpacing();
+	const [spacing, { xs: xsSpacing }] = useSpacing();
 
 	const { code = 404, title, subtitle, renderActions } = props;
 
 	return (
 		<Center width='100%' height='100%' minHeight='inherit'>
-			<VStack spacing={spacing} p={padding}>
+			<VStack spacing={spacing} p={xsSpacing}>
 				<Show breakpoint={`(max-width: ${theme.breakpoints.md})`}>
-					<VStack alignItems='flex-start' divider={<Divider colorMode={colorMode} />} spacing={padding}>
-						<Code code={code} />
-						<Description title={title} subtitle={subtitle} />
+					<VStack alignItems='flex-start' divider={<Divider />} spacing={xsSpacing}>
+						<ErrorCode code={code} />
+						<ErrorDescription title={title} subtitle={subtitle} />
 					</VStack>
 				</Show>
 				<Show breakpoint={`(min-width: ${theme.breakpoints.md})`}>
 					<HStack
 						alignItems='stretch'
 						justifyContent='stretch'
-						divider={<Divider colorMode={colorMode} orientation='vertical' />}
-						spacing={padding}
+						divider={<Divider orientation='vertical' />}
+						spacing={xsSpacing}
 					>
-						<Code code={code} />
-						<Description title={title} subtitle={subtitle} />
+						<ErrorCode code={code} />
+						<ErrorDescription title={title} subtitle={subtitle} />
 					</HStack>
 				</Show>
 
 				{renderActions && (
-					<HStack width='100%' alignItems='flex-start' spacing={padding}>
-						{renderActions({ color, colorMode, size: 'md', variant: 'contained' })}
+					<HStack width='100%' alignItems='flex-start' spacing={xsSpacing}>
+						{renderActions({ size: 'md', variant: 'contained' })}
 					</HStack>
 				)}
 			</VStack>
