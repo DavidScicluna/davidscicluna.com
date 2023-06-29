294e9a06b04710a8c0b9178cbb0fd42dd2da4b8d
DavidScicluna <dave_nostra@hotmail.com>
2023-06-29 22:25:11 +0200

    Fix: useSpacing

--- src/common/hooks/useSpacing.ts
+++ src/common/hooks/useSpacing.ts
@@ -2,14 +2,16 @@ import { Space, useTheme } from '@davidscicluna/component-library';
 
 import { useMediaQuery } from '@chakra-ui/react';
 
-const useSpacing = (): Space => {
+type UseSpacingReturn = [Space, Record<'xs' | 'sm' | 'md' | 'xl', Space>];
+
+const useSpacing = (): UseSpacingReturn => {
 	const theme = useTheme();
 
 	const [isSm] = useMediaQuery(`(min-width: ${theme.breakpoints.sm})`);
 	const [isMd] = useMediaQuery(`(min-width: ${theme.breakpoints.md})`);
 	const [isXl] = useMediaQuery(`(min-width: ${theme.breakpoints.xl})`);
 
-	return isXl ? 5 : isMd ? 4 : isSm ? 3 : 2;
+	return [isXl ? 5 : isMd ? 4 : isSm ? 3 : 2, { xs: 2, sm: 3, md: 4, xl: 5 }];
 };
 
 export default useSpacing;
