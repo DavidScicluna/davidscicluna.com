0952f4d51f9f5ce16b47153c04fcf30d29e7ef46
DavidScicluna <dave_nostra@hotmail.com>
2023-06-30 21:36:52 +0200

    Fix: MailOverlay

--- src/components/MailOverlay/index.tsx
+++ src/components/MailOverlay/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 
-import { ExternalLink, LinkProps as MailOverlayProps } from '@davidscicluna/component-library';
+import { Link, LinkProps as MailOverlayProps } from '@davidscicluna/component-library';
 
-const email = import.meta.env.VITE_EMAIL_URL;
+const email = process.env.VITE_EMAIL_URL;
 
 const MailOverlay: FC<MailOverlayProps> = ({ children }) => {
-	return <ExternalLink href={`mailto:${email}`}>{children}</ExternalLink>;
+	return <Link href={`mailto:${email}`}>{children}</Link>;
 };
 
 export default MailOverlay;
