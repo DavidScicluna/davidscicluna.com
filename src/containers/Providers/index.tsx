ea3c93f77153d4c81265f8f4e034d89dc3bd894e
DavidScicluna <dave_nostra@hotmail.com>
2023-06-30 23:36:59 +0200

    Fix: Providers

--- src/containers/Providers/index.tsx
+++ src/containers/Providers/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 
+import { DSCLProviderProps as ProvidersProps } from '@davidscicluna/component-library';
+
 import { Provider as StoreProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from '../../store';
 import Provider from '../Provider';
 
-const Providers: FC = () => {
+const Providers: FC<ProvidersProps> = ({ children }) => {
 	return (
 		<StoreProvider store={store}>
 			<PersistGate loading={null} persistor={persistor}>
