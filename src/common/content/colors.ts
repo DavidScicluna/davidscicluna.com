46869c2b866b76b857b08fc70c9b4d960f3802df
DavidScicluna <dave_nostra@hotmail.com>
2023-07-01 20:21:37 +0200

    Feature: colors

---
+++ src/common/content/colors.ts
@@ -0,0 +1,21 @@
+import { AppColors } from '@davidscicluna/component-library';
+
+const colors: AppColors = [
+	'red',
+	'pink',
+	'purple',
+	'deep_purple',
+	'indigo',
+	'blue',
+	'light_blue',
+	'cyan',
+	'teal',
+	'green',
+	'light_green',
+	'lime',
+	'yellow',
+	'orange',
+	'deep_orange'
+];
+
+export default colors;
