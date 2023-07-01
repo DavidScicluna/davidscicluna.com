b7cda618945263a2cc0b8477c7aba92904fd5d69
DavidScicluna <dave_nostra@hotmail.com>
2023-07-01 18:15:50 +0200

    Removed HoveringOverlay

--- src/components/HoveringOverlay/types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode, MouseEvent as ME } from 'react';
-
-import { BoxProps } from '@chakra-ui/react';
-
-export type HoveringOverlayMouseEvent = ME<HTMLDivElement, globalThis.MouseEvent>;
-
-export type HoveringOverlayProps = Omit<BoxProps, 'children'> & {
-	children: (isHovering: boolean) => ReactNode;
-};
