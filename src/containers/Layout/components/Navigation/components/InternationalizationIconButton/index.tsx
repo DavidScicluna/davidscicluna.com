95d954c872e569c60809f703e3189b260ef90e61
DavidScicluna <dave_nostra@hotmail.com>
2023-07-01 20:17:22 +0200

    Fix: Layout - Navigation - InternationalizationIconButton

--- src/containers/Layout/components/Navigation/components/InternationalizationIconButton/index.tsx
+++ src/containers/Layout/components/Navigation/components/InternationalizationIconButton/index.tsx
@@ -1,44 +1,46 @@
 import { FC } from 'react';
 
-import { Tooltip, IconButton, IconButtonIcon } from '@davidscicluna/component-library';
-
-import { useBoolean } from '@chakra-ui/react';
+import {
+	Tooltip,
+	IconButton,
+	IconButtonIcon,
+	HoverOverlay,
+	useGetThemeAppearance
+} from '@davidscicluna/component-library';
 
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 
-import { useUserTheme } from '../../../../../../common/hooks';
 import { toggleInternationalizationModal } from '../../../../../../store/slices/Modals';
 
 const InternationalizationIconButton: FC = () => {
-	const { colorMode } = useUserTheme();
+	const { colorMode } = useGetThemeAppearance();
 
 	const dispatch = useDispatch();
 
 	const { t } = useTranslation();
 
-	const [isHovering, setIsHovering] = useBoolean();
-
 	return (
-		<Tooltip
-			aria-label={`${t('layout.navigation.internationalizationIconButton.aria-label.tooltip')}`}
-			colorMode={colorMode}
-			isOpen={isHovering}
-			placement='bottom-end'
-			label={`${t('layout.navigation.internationalizationIconButton.tooltip')}`}
-		>
-			<IconButton
-				aria-label={`${t('layout.navigation.internationalizationIconButton.aria-label.button')}`}
-				color={colorMode === 'light' ? 'black' : 'white'}
-				colorMode={colorMode}
-				onMouseEnter={() => setIsHovering.on()}
-				onMouseLeave={() => setIsHovering.off()}
-				onClick={() => dispatch(toggleInternationalizationModal(true))}
-				variant='icon'
-			>
-				<IconButtonIcon icon='language' category='outlined' />
-			</IconButton>
-		</Tooltip>
+		<HoverOverlay>
+			{({ isHovering }) => (
+				<Tooltip
+					aria-label={`${t('layout.navigation.internationalizationIconButton.aria-label.tooltip')}`}
+					color='gray'
+					isOpen={isHovering}
+					placement='bottom-end'
+					label={`${t('layout.navigation.internationalizationIconButton.tooltip')}`}
+				>
+					<IconButton
+						aria-label={`${t('layout.navigation.internationalizationIconButton.aria-label.button')}`}
+						color={colorMode === 'light' ? 'black' : 'white'}
+						onClick={() => dispatch(toggleInternationalizationModal(true))}
+						variant='icon'
+					>
+						<IconButtonIcon icon='language' category='outlined' />
+					</IconButton>
+				</Tooltip>
+			)}
+		</HoverOverlay>
 	);
 };
 
