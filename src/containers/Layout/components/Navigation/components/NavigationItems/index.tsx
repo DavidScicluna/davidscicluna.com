1f507332ba44bdd656760fce6c4994c6cbe8eeda
DavidScicluna <dave_nostra@hotmail.com>
2023-07-01 20:17:31 +0200

    Fix: Layout - Navigation - NavigationItems

--- src/containers/Layout/components/Navigation/components/NavigationItems/index.tsx
+++ src/containers/Layout/components/Navigation/components/NavigationItems/index.tsx
@@ -1,39 +1,37 @@
 import { FC } from 'react';
 
-import { useLocation } from 'react-router';
+import { Link } from 'gatsby';
 
-import { useTheme, InternalLink, Button } from '@davidscicluna/component-library';
+import { useTheme, Button, useGetThemeAppearance } from '@davidscicluna/component-library';
 
 import { useMediaQuery, HStack } from '@chakra-ui/react';
 
-import { useGetNavItems, useUserTheme } from '../../../../../../common/hooks';
+import { useGetNavItems } from '../../../../../../common/hooks';
 
 const NavigationItems: FC = () => {
 	const theme = useTheme();
-	const { color, colorMode } = useUserTheme();
+
+	const { color } = useGetThemeAppearance();
 
 	const [isSm] = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
 	const [isMd] = useMediaQuery(`(max-width: ${theme.breakpoints.md})`);
-
-	const location = useLocation();
 
 	const navItems = useGetNavItems();
 
 	return (
 		<HStack spacing={0}>
-			{navItems.map(({ title, location: l, getIsActive }, index) => {
+			{navItems.map(({ title, pathname, getIsActive }) => {
 				const isActive = getIsActive(location);
 				return (
-					<InternalLink key={index} to={{ ...l }} isDisabled={isActive}>
+					<Link key={pathname} to={pathname}>
 						<Button
 							color={isActive ? color : 'gray'}
-							colorMode={colorMode}
 							size={isSm ? 'xs' : isMd ? 'sm' : 'md'}
 							variant='text'
 						>
 							{title}
 						</Button>
-					</InternalLink>
+					</Link>
 				);
 			})}
 		</HStack>
