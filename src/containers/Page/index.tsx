b02253515ec7a05d17db8e508c562f6de69fb7e1
DavidScicluna <dave_nostra@hotmail.com>
2023-07-02 15:53:43 +0200

    Fix: Page

--- src/containers/Page/index.tsx
+++ src/containers/Page/index.tsx
@@ -6,15 +6,15 @@ import { useWindowSize } from 'rooks';
 
 import { useLayoutContext } from '../Layout/common/hooks';
 
-import { PageProps } from './types';
+import { PageProps } from './common/types';
 
 const Page: FC<PageProps> = ({ children }) => {
-	const { height } = useLayoutContext();
+	const { navigationHeight } = useLayoutContext();
 
 	const { innerHeight: windowHeight } = useWindowSize();
 
 	return (
-		<Box width='100%' minHeight={`calc(${windowHeight ? `${windowHeight}px` : '100vh'} - ${height}px)`}>
+		<Box width='100%' minHeight={`calc(${windowHeight ? `${windowHeight}px` : '100vh'} - ${navigationHeight}px)`}>
 			{children}
 		</Box>
 	);
