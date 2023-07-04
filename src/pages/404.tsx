e7965f5a162da4af688eb4326f622253f221cc97
DavidScicluna <dave_nostra@hotmail.com>
2023-07-04 21:22:00 +0200

    Fix: 404 page - Added Meta

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,6 +2,7 @@ import { Button, useTheme } from '@davidscicluna/component-library';
 
 import { Center, useMediaQuery } from '@chakra-ui/react';
 
+import { Meta } from '@/components';
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 import { useWindowSize } from 'rooks';
@@ -21,34 +22,37 @@ const Error404Page = () => {
 	const { innerHeight: windowHeight = 0 } = useWindowSize();
 
 	return (
-		<Center
-			width='100%'
-			minHeight={`calc(${windowHeight ? `${windowHeight}px` : '100vh'} - ${navigationHeight}px)`}
-		>
-			<Error
-				code={404}
-				title={`${t('error404.title')}`}
-				subtitle={`${t('error404.subtitle')}`}
-				renderActions={(props) => (
-					<>
-						<Link href='/'>
-							<Button {...props} isFullWidth={isSm}>
-								{`${t('error404.action1')}`}
-							</Button>
-						</Link>
+		<>
+			<Meta title={`${t('error404.pageTitle')}`} />
+			<Center
+				width='100%'
+				minHeight={`calc(${windowHeight ? `${windowHeight}px` : '100vh'} - ${navigationHeight}px)`}
+			>
+				<Error
+					code={404}
+					title={`${t('error404.title')}`}
+					subtitle={`${t('error404.subtitle')}`}
+					renderActions={(props) => (
+						<>
+							<Link href='/'>
+								<Button {...props} isFullWidth={isSm}>
+									{`${t('error404.action1')}`}
+								</Button>
+							</Link>
 
-						<Button
-							{...props}
-							isFullWidth={isSm}
-							onClick={() => window.location.reload()}
-							variant='outlined'
-						>
-							{`${t('error404.action2')}`}
-						</Button>
-					</>
-				)}
-			/>
-		</Center>
+							<Button
+								{...props}
+								isFullWidth={isSm}
+								onClick={() => window.location.reload()}
+								variant='outlined'
+							>
+								{`${t('error404.action2')}`}
+							</Button>
+						</>
+					)}
+				/>
+			</Center>
+		</>
 	);
 };
 
